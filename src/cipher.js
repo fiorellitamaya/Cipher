3678b8411c3f20139f602f2ed2c9898a93790ffe
fiorellitamaya <fiorellitamaya@hotmail.com>
2022-06-06 23:36:23 -0500

    Ultima actualizacion

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,44 @@
 const cipher = {
-  // ...
+  encode: function(desplazamiento,mensaje){
+    //console.log(desplazamiento,mensaje)
+      
+    let resultado = ""
+    mensaje = mensaje.toUpperCase();
+  
+    for (let i=0; i<mensaje.length; ++i){
+      //console.log(mensaje[i])
+      let posicionLetra = (mensaje.charCodeAt(i))
+      console.log(posicionLetra >=65 && posicionLetra<=90)
+      let posicionCifrada = ((posicionLetra-65+desplazamiento)%26+65)
+      //console.log((posicionLetra-65+desplazamiento)%26+65)
+      let letraCifrada = (String.fromCharCode(posicionCifrada))
+      //console.log(letraCifrada)
+      resultado = resultado + letraCifrada 
+       }
+  //console.log(resultado)
+  return resultado
+  },
+
+
+  decode: function(desplazamiento, mensaje){
+    //console.log(desplazamiento,mensaje)
+    let resultado = ""
+    mensaje = mensaje.toUpperCase();
+
+    for (let i=0; i<mensaje.length; ++i){
+      //console.log(mensaje[i])
+      let posicionLetra = (mensaje.charCodeAt(i))
+     // console.log(mensaje.charCodeAt(i))
+      let posicionCifrada = ((posicionLetra+65-desplazamiento)%26+65)
+      //console.log((posicionLetra+65-desplazamiento)%26+65)
+      let letraCifrada = (String.fromCharCode(posicionCifrada))
+      //console.log(letraCifrada)
+      resultado = resultado + letraCifrada 
+     }
+      //console.log(resultado)
+      return resultado
+  }
 };
 
 export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import cipher from './cipher.js';
 
+function cifrar(){
+let mensaje = document.getElementById("mensaje1").value;
+let desplazamiento = parseInt(document.getElementById("desplazamientos").value);
+document.getElementById("resultado").value = cipher.encode(desplazamiento, mensaje)
+}
+
+document.getElementById("cifrar").addEventListener('click',cifrar)
+
+function descifrar(){
+let mensaje = document.getElementById("mensaje1").value;
+let desplazamiento = parseInt(document.getElementById("desplazamientos").value);
+document.getElementById("resultado").value = cipher.decode(desplazamiento, mensaje)
+}
+
+document.getElementById("descifrar").addEventListener('click',descifrar)
+
+function limpiar(){
+    document.getElementById("mensaje1").value="";
+    document.getElementById("desplazamientos").value="";
+    document.getElementById("resultado").value=""
+}
+document.getElementById("limpiar").addEventListener('click',limpiar)
+
+function copiar(){
+let contenido = document.getElementById("resultado");
+contenido.select()
+//Es inseguro
+//document.execCommand("copy")
+navigator.clipboard.writeText(contenido.value)
+}
+document.getElementById("copiar").addEventListener('click',copiar)
+
+
 console.log(cipher);
